56dfb64a0633ceb0b420e8f89514a09f14906336
Yash Gohel <gohelyash1901@gmail.com>
2025-02-21 09:38:05 +0530

    Loader->Tostify

--- src/Components/Apis/ApiDemo1.jsx
+++ src/Components/Apis/ApiDemo1.jsx
@@ -1,20 +1,44 @@
 import axios from 'axios';
 import  'react'
 import { useEffect, useState } from 'react';
+import { ComonLoader } from '../ComonLoader';
+import { Bounce, toast, ToastContainer } from 'react-toastify';
 
 export const ApiDemo1 = () => {
 
     const [message, setmessage] = useState("");
+    const [isLoader, setisLoader] = useState(false);
     const [users, setusers] = useState([]);
 
     const getUser = async()=>{
+        setisLoader(true);
         const res  = await axios.get("https://node5.onrender.com/user/user");
-        console.log(res);
-        console.log(res.data);        
-        console.log(res.data.message);
         setmessage(res.data.message);
-        console.log(res.data.data);
         setusers(res.data.data); 
+        setisLoader(false);
+
+
+    }
+
+    const deleteUser = async(id)=>{
+        const res = await axios.delete("https://node5.onrender.com/user/user/"+id);
+        console.log(res);
+        if (res.status == 204) {
+            //alert("user deleted..");
+            toast.error('record deleted !!', {
+              position: "top-left",
+              autoClose: 3000,
+              hideProgressBar: false,
+              closeOnClick: false,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "dark",
+              transition: Bounce,
+              });
+            getUser(); //get -->
+          }
+        
     }
     useEffect(() => {
         getUser();
@@ -22,7 +46,24 @@ export const ApiDemo1 = () => {
     
 
   return (
+    
     <div style={{textAlign:"center"}}>
+          <ToastContainer
+        position="top-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Bounce}
+      />
+        {
+            isLoader == true && <ComonLoader></ComonLoader>
+        }
         <h1>Api Demo1</h1>
         {/* <button onClick={getUser}>Get User</button><br></br> */}
         {/* {message}
@@ -38,16 +79,19 @@ export const ApiDemo1 = () => {
                     <th>Age</th>
                     <th>Email</th>
                     <th>isActive</th>
+                    <th>Action</th>
                 </tr>
             </thead>
             <tbody>
                {
                 users?.map((user)=>{
                    return <tr>
+                        <td>{user._id}</td>
                         <td>{user.name}</td>
                         <td>{user.age}</td>
                         <td>{user.email}</td>
                         <td>{user.isActive ? "true":"false"}</td>
+                        <td><button onClick={()=>{deleteUser(user._id)}} className="btn btn-danger">DELETE</button></td>
                     </tr>
                 })
                }
